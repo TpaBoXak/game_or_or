ba705d3e5cb2bfb12a10ba747992059bccda7fc3
Арсений <gl.arseniy@mail.ru>
2024-08-04 01:03:41 +0300

    db helper and data base conf

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from models import DataBaseHelper
+from config import settings
+
+db_helper = DataBaseHelper(
+        url=str(settings.db.url),
+        echo=settings.db.echo,
+        echo_pool=settings.db.echo_pool,
+        pool_size=settings.db.pool_size,
+        max_overflow=settings.db.max_overflow,
+)
